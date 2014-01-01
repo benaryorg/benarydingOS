2b97ea7c99ceee13d040dc6be1e8a4fa85753b0c
benaryorg <binary@benary.org>
2014-01-01 22:10:41 +0100

    Added a few Files and organized the whole Project a bit Also the new Compiler is now "cc"

---
+++ functions.c
@@ -0,0 +1,6 @@
+#include "header.h"
+
+void kputchar(char ch)
+{
+	
+}
---
+++ header.h
@@ -0,0 +1,8 @@
+#ifndef __BENARYDINGOS_HEADER__
+#define __BEANRYDINGOS_HEADER__
+
+#define BUFFER_TEXT ((const char *)0xb8000)
+
+void kputchar(char);
+
+#endif
