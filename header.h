f2b05e0d2607a94a304df959bad075eb68cc2161
benaryorg <binary@benary.org>
2014-01-01 22:26:29 +0100

    Programmed the kputchar and a Colormanagement

--- header.h
+++ header.h
@@ -6,8 +6,11 @@ typedef __builtin_va_list       va_list;
 #define va_arg(ap, type)        __builtin_va_arg(ap, type)
 #define va_end(ap)              __builtin_va_end(ap)
 
-#define BUFFER_TEXT ((const char *)0xb8000)
+#define BUFFER_TEXT ((char *)0xb8000)
 
 void kputchar(char);
+char color(char,char);
+void setcolor(char);
+char getcolor(void);
 
 #endif
