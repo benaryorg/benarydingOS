a02e398ec94d9c90660770994f880f6e54c5be33
nilsding <nilsding@nilsding.org>
2014-01-03 00:01:05 +0100

    implemented a simple printf which can only use %c and %s as of now

--- src/c/output/printf.c
+++ src/c/output/printf.c
@@ -3,15 +3,37 @@
 int printf(const char *format,...)
 {
 	const char *p;
+	int i, count = 0;
+	char *s;
+	
 	va_list list;
 	va_start(list,format);
 	
 	for (p = format; *p != 0x00; p++) {
+		if (*p != '%') {
+			if (putchar(*p)) {
+				count++;
+			}
+			continue;
+		}
 		
+		switch (*++p) {
+			case 'c':
+				i = va_arg(list, int);
+				if (putchar(i)) {
+					count++;
+				}
+				break;
+			
+			case 's':
+				s = va_arg(list, char*);
+				count += puts(s);
+				break;
+		}
 	}
 	
 	va_arg(list,int);
 	
 	va_end(list);
-	return 0;//Upon successful return, these functions return the number of characters printed (excluding the null byte used to end output to strings).
+	return count;
 }
