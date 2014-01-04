605b85890ab57ab9c6c8231ad7e70d206b8c2e81
nilsding <nilsding@nilsding.org>
2014-01-04 01:52:18 +0100

    sgfhsdgjkhdflkg

--- geniso.sh
+++ geniso.sh
@@ -7,10 +7,15 @@
 # you need to emerge sys-boot/grub:0 if you want to want that script to work
 # oh and app-cdr/cdrtools! that one is needed for mkisofs
 
-GENISOARGS='-R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -V "BDOS" -o benarydingOS.iso iso'
+GENISOARGS='-R -b boot/grub/stage2_eltorito -no-emul-boot -boot-load-size 4 -boot-info-table -V "BDOS" -o build/iso/benarydingOS.iso build/iso'
 
-mkdir -p iso/boot/grub
-cp /lib/grub/i386-pc/stage2_eltorito iso/boot/grub/
+printf "\033[32;1m * \033[0mCreating directories\033[0m\n"
+mkdir -p build/iso/boot/grub
+
+printf "\033[32;1m * \033[0mCopying grub stage2\033[0m\n"
+cp /lib/grub/i386-pc/stage2_eltorito build/iso/boot/grub/
+
+printf "\033[32;1m * \033[0mWriting menu.lst\033[0m\n"
 echo "default 0
 timeout 30
 
@@ -19,6 +24,9 @@ kernel /boot/kernel
 
 title Boot from hard disk
 chainloader (hd0)+1
-" > iso/boot/grub/menu.lst
-cp build/kernel iso/boot/kernel
+" > build/iso/boot/grub/menu.lst
+printf "\033[32;1m * \033[0mCopying kernel\033[0m\n"
+cp build/kernel build/iso/boot/kernel
+printf "\033[32;1m * \033[0mStarting mkisofs or genisoimage\033[0m\n"
 genisoimage $GENISOARGS || mkisofs $GENISOARGS
+printf "\033[32;1m * \033[0mThe iso was built in \033[1mbuild/iso/benarydingOS.iso\033[0m\n"
