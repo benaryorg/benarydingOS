3510b33bb749e2d12f978d281baa28a8458c95f5
benaryorg <binary@benary.org>
2014-01-04 02:43:24 +0100

    init changed to test malloc it is fucking slow!

--- src/c/main.c
+++ src/c/main.c
@@ -2,9 +2,39 @@
 
 int init(multiboot_info_t *mb_info)
 {
-	physmeminit(mb_info);
 	cleardisplay();
-	
+
+	printf("Started\n");
+
+	physmeminit(mb_info);
+
+	printf("Inited\n");
+
+	int *f[10]={};
+	int i,j;
+	for(i=0;i<10;i++)
+	{
+		f[i]=malloc(sizeof(int)*100);
+
+		printf("Allocated %d\n");
+
+		for(j=0;j<100;j++)
+		{
+			f[i][j]=j;
+		}
+	}
+
+	for(i=0;i<10;i++)
+	{
+		for(j=0;j<100;j++)
+		{
+			if(f[i][j]!=j)
+			{
+				printf("Fehler\n");
+			}
+		}
+	}
+
 	printf("Hello, world!\n");
 	
 	return 0;
