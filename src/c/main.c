26d76d084e2ae38c6b66d9691c0d9646d0068738
nilsding <nilsding@nilsding.org>
2014-01-04 21:53:29 +0100

    .

--- src/c/main.c
+++ src/c/main.c
@@ -6,20 +6,6 @@ int main(void)
 	puts("BenarydingOS Running!");
 	puts("Memorydump:");
 	memdump();
-<<<<<<< HEAD
-// 	cleardisplay();
-// 	int i;
-// 	for(i=1;i<24;i++)
-// 	{
-// 		printf("%d  ",i);
-// 	}
-	printf("\n-1 in hex: %x\n",-1);
-	printf("-32 in hex: %x\n",-32);
-	printf("-32 in binary: %b\n",-32);
-	printf("\n1 in hex: %x\n",1);
-	printf("32 in hex: %x\n",32);
-	printf("32 in binary: %b\n",32);
-=======
->>>>>>> 1d26fe9c968337ec0327222e1b0a209900da0606
+
 	return 0;
 }
