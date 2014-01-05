7cf048fc91a40864b8a7f44f7480544b49f7629c
benaryorg <binary@benary.org>
2014-01-05 05:41:27 +0100

    Changed Interrupthandler to do Kernelpanics

--- src/c/interrupts/handler.c
+++ src/c/interrupts/handler.c
@@ -2,14 +2,13 @@
 
 void int_handler(cpu_state_t *cpu)
 {
-	if(cpu->intr==0x20)
-	{
-		puts("Timer");
-	}
 	if(cpu->intr<0x20)
 	{
 		printf("Exception %d\n",(int)cpu->intr);
-//		asm volatile("cli");
-//		kernelpanic("Interrupt");
+		kernelpanic("Interrupt");
+	}
+	if(cpu->intr==0x20)
+	{
+		puts("Timer");
 	}
 }
