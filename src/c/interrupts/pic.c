0c7edb39c09c76ebcd973bd014a576e03be96350
benaryorg <binary@benary.org>
2014-01-05 17:14:30 +0100

    Changed a bit

--- src/c/interrupts/pic.c
+++ src/c/interrupts/pic.c
@@ -3,16 +3,16 @@
 void pic_init(void)
 {
 	//Master-PIC initialisieren
-	outb(0x20, 0x11); // Initialisierungsbefehl fuer den PIC
-	outb(0x21, 0x20); // Interruptnummer fuer IRQ 0
-	outb(0x21, 0x04); // An IRQ 2 haengt der Slave
-	outb(0x21, 0x01); // ICW 4
+	outb(0x20, 0x11);// Initialisierungsbefehl fuer den PIC
+	outb(0x21, 0x20);// Interruptnummer fuer IRQ 0
+	outb(0x21, 0x04);// An IRQ 2 haengt der Slave
+	outb(0x21, 0x01);// ICW 4
 
 	// Slave-PIC initialisieren
-	outb(0xa0, 0x11); // Initialisierungsbefehl fuer den PIC
-	outb(0xa1, 0x28); // Interruptnummer fuer IRQ 8
-	outb(0xa1, 0x02); // An IRQ 2 haengt der Slave
-	outb(0xa1, 0x01); // ICW 4
+	outb(0xa0, 0x11);// Initialisierungsbefehl fuer den PIC
+	outb(0xa1, 0x28);// Interruptnummer fuer IRQ 8
+	outb(0xa1, 0x02);// An IRQ 2 haengt der Slave
+	outb(0xa1, 0x01);// ICW 4
 
 	// Alle IRQs aktivieren (demaskieren)
 	outb(0x20, 0x0);
--- src/c/main.c
+++ src/c/main.c
@@ -7,6 +7,9 @@ int main(void)
 	puts("Memorydump:");
 	memdump();
 	cleardisplay();
-	while(1);
+	while(1)
+	{
+		asm volatile("int $0x20");
+	}
 	return 0;
 }
