9bd9e64745e53727a23a27404b11045a903db0b8
benaryorg <benedikt.bre3@gmail.com>
2014-01-29 10:52:02 +0100

    Changed memtest parameters to just run the System

--- kernel/c/init.c
+++ kernel/c/init.c
@@ -14,7 +14,7 @@ void init(multiboot_info_t *mb_info)
 	physmeminit(mb_info);
 	puts("Physical Memory initialised");
 	printf("Loaded %d Modules\n",(int)mb_info->mbs_mods_count);
-	if(memtest(128,128))
+	if(memtest(64,64))
 	{
 		setfgcolor(COLOR_LIGHT_RED);
 		memdump();
