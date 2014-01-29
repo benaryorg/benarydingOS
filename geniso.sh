89e8e4bb32a367836dd87164d376645b5dfc7edb
benaryorg <binary@benary.org>
2014-01-29 20:17:48 +0100

    Added the path for Debian/Ubuntu

--- geniso.sh
+++ geniso.sh
@@ -23,6 +23,8 @@ printf "\033[32;1m * \033[0mCreating directories\033[0m\n"
 mkdir -p build/iso/boot/grub
 
 printf "\033[32;1m * \033[0mCopying grub stage2\033[0m\n"
+
+#If you are using Ubuntu/Debian it is possible, the right path to the stage2_eltorito is "/usr/lib/grub/i386-pc/stage2_eltorito"
 cp /lib/grub/i386-pc/stage2_eltorito build/iso/boot/grub/
 
 echo "default 0
