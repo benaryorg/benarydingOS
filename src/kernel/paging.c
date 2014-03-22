2d453ef4447378d70f4c577bd4472642215173c1
benaryorg <benedikt.bre3@gmail.com>
2014-03-23 00:03:14 +0100

    Paging does werk!

--- src/kernel/paging.c
+++ src/kernel/paging.c
@@ -2,61 +2,63 @@
 
 void paging_init(void)
 {
-	static page_context_t *kernel_ctx;
+    static page_context_t *kernel_ctx;
 
-	uint32_t cr0;
-	int i;
+    uint32_t cr0;
+    int i;
 
-	kernel_ctx=page_mk_context();
+    kernel_ctx=page_mk_context();
 
-	for(i=0x1000;i<4*1024*1024;i+=0x1000)
-	{
-		page_map(kernel_ctx,i,i,PTE_PRESENT|PTE_WRITE);
-	}
-	page_map(kernel_ctx,(uint32_t)kernel_ctx,(uint32_t)kernel_ctx,PTE_PRESENT|PTE_WRITE);
-	page_map(kernel_ctx,(uint32_t)kernel_ctx->pagedir,(uint32_t)kernel_ctx->pagedir,PTE_PRESENT|PTE_WRITE);
-	page_map(kernel_ctx,0xB8000,0xB8000,PTE_PRESENT|PTE_WRITE);
+    for(i=0x1000;i<4*1024*1024;i+=0x1000)
+    {
+        page_map(kernel_ctx,i,i,PTE_PRESENT|PTE_WRITE);
+    }
+    page_map(kernel_ctx,(uint32_t)kernel_ctx,(uint32_t)kernel_ctx,PTE_PRESENT|PTE_WRITE);
+    page_map(kernel_ctx,(uint32_t)kernel_ctx->pagedir,(uint32_t)kernel_ctx->pagedir,PTE_PRESENT|PTE_WRITE);
+    page_map(kernel_ctx,0xB8000,0xB8000,PTE_PRESENT|PTE_WRITE);
     
-	page_activate_context(kernel_ctx);
+    page_activate_context(kernel_ctx);
 
-	asm volatile("mov %%cr0, %0" : "=r" (cr0));
-	cr0|=(1<<31);
-	asm volatile("mov %0, %%cr0" : : "r" (cr0));
+    asm volatile("mov %%cr0, %0" : "=r" (cr0));
+    cr0|=(1<<31);
+    asm volatile("mov %0, %%cr0" : : "r" (cr0));
 }
 
 page_context_t *page_mk_context(void)
 {
-	page_context_t *c=physmallocblock();//(sizeof(page_context_t));
-	page_map(c,(uint32_t)c,(uint32_t)c,PTE_PRESENT|PTE_WRITE);
-	c->pagedir=(pagedir_t)physmallocblock();
-	memset(c->pagedir,0,4096);
-	return c;
+    page_context_t *c=physmallocblock();//(sizeof(page_context_t));
+    page_map(c,(uint32_t)c,(uint32_t)c,PTE_PRESENT|PTE_WRITE);
+    c->pagedir=(pagedir_t)physmallocblock();
+    memset(c->pagedir,0,4096);
+    return c;
 }
 
 void page_activate_context(page_context_t *c)
 {
-	asm volatile("movl %0, %%cr3" : :  "r" (c->pagedir));
+    asm volatile("movl %0, %%cr3" : :  "r" (c->pagedir));
 }
 
 void invalpage(uint32_t virt)
 {
-	asm volatile("invlpg %0" : : "m" (*(char *)virt));
+    asm volatile("invlpg %0" : : "m" (*(char *)virt));
 }
 
 void page_map(page_context_t *c,uint32_t virt,uint32_t phys,uint32_t flags)
 {
-	if(!virt||((virt|phys)&0xFFF))
-	{
-		kernelpanic("Evil Flags!");
-		return;
-	}
-	int i=virt>>12;
-	if(!c->pagedir[i/1024])
-	{
-		c->pagedir[i/1024]=(pagetable_t)physmallocblock();
-		memset((void *)c->pagedir[i/1024],0,4096);
+    if(!virt||((virt|phys)&0xFF))
+    {
+//        kernelpanic("Evil Flags!");
+        printf("Evil Flags!");
+        return;
+    }
+    int i=virt>>12;
+    if(!(((uint32_t)c->pagedir[i/1024])&0x1))
+    {
+        c->pagedir[i/1024]=(pagetable_t)(((uint32_t)physmallocblock())|PTE_PRESENT);
+        memset((void *)c->pagedir[i/1024],0,4096);
         page_map(c,(uint32_t)c->pagedir[i/1024],(uint32_t)c->pagedir[i/1024],PTE_PRESENT|PTE_WRITE);
-	}
-	c->pagedir[i/1024][i%1024]=(uint32_t *)(phys|flags);
-	invalpage(virt);
+    }
+    pagetable_t table=(pagetable_t)(((uint32_t)c->pagedir[i/1024])&~0xFF);
+    table[i%1024]=(uint32_t *)(phys|flags);
+    invalpage(virt);
 }
