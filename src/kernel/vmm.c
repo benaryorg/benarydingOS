a43037073eee04617412e6cfc145fc898de027da
benaryorg <benedikt.bre3@gmail.com>
2014-03-23 17:35:50 +0100

    Problem solved! Next Problem:     Paging is initialized without running Paging!     Therefore we have a problem with mallocblocks calls without     activated Paging!

--- src/kernel/vmm.c
+++ src/kernel/vmm.c
@@ -8,13 +8,13 @@ void *mallocblocks(page_context_t *c,int blocks)
     }
     char tmp=0;
     int i,j,k;
-    for(i=0x200;i<0x10000;i++)
+    for(i=0x10;i<0x10000;i++)
     {
         if(((uint32_t)c->pagedir[i/1024])&1)
         {
             for(j=0;j<1024-blocks;j++)
             {
-                for(tmp=k=0;k<blocks&&tmp;k++)
+                for(tmp=k=0;k<blocks&&!tmp;k++)
                 {
                     if(((uint32_t)c->pagedir[i/1024][j+k])&1)
                     {
@@ -27,13 +27,13 @@ void *mallocblocks(page_context_t *c,int blocks)
                     {
                         page_map(c,(uint32_t)((i*1024+j+k)<<12),(uint32_t)physmallocblock(),PTE_PRESENT|PTE_WRITE);
                     }
-//                    printf("%x\n",c->pagedir[i/1024][j+k]);
-                    return (void *)(((uint32_t)c->pagedir[i/1024][j+k])&~0xFFF);
+                    printf("%x\n",c->pagedir[i/1024][j+k]);
+                    return (void *)((i*1024+j+k)<<12); //(void *)(((uint32_t)c->pagedir[i/1024][j+k])&~0xFFF);
                 }
             }
         }
     }
-    for(i=0x200;1;i++)
+    for(i=0x10;i<0x10000;i++)
     {
         if(!(((uint32_t)c->pagedir[i/1024])&1))
         {
@@ -44,6 +44,7 @@ void *mallocblocks(page_context_t *c,int blocks)
                 page_map(c,(uint32_t)((i*1024+j)<<12),(uint32_t)physmallocblock(),PTE_PRESENT|PTE_WRITE);
             }
 */
+//            printf("%x\n",c->pagedir[i/1024]);
             return mallocblocks(c,blocks);
         }
     }
